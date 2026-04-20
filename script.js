b25a858507c89aecf7b495c1ae8d064d920b800f
Eczbek <eczbek.void@gmail.com>
2026-04-20 15:14:24 -0400

    Add languages, compilers, output colors, buttons

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { AnsiUp } from 'https://cdn.jsdelivr.net/npm/ansi_up@6.0.6/ansi_up.js';
+
 function debounce(wait, func) {
 	let timeout;
 	return (...args) => {
@@ -8,6 +10,17 @@ function debounce(wait, func) {
 
 const tabs = document.querySelectorAll('.tab');
 const panels = document.querySelectorAll('.panel');
+const status = document.querySelector('#status');
+const source = document.querySelector('#source');
+const output = document.querySelector('#output > pre > code');
+const flags = document.querySelector('#flags');
+const recompile = document.querySelector('#recompile');
+const lang_select = document.querySelector('#lang-select');
+const compiler_select = document.querySelector('#compiler-select');
+const short_link = document.querySelector('#short-link');
+
+require.config({ paths: { 'vs': 'https://cdn.jsdelivr.net/npm/monaco-editor@0.39.0/min/vs' }});
+
 for (const tab of tabs) {
 	tab.addEventListener('click', () => {
 		for (const tab of tabs) {
@@ -20,88 +33,193 @@ for (const tab of tabs) {
 	});
 }
 
-const output = document.querySelector('#output > pre > code');
-const status = document.querySelector('#status');
-const flags = document.querySelector('#flags');
+const config = {
+	code: '',
+	lang: 'c++',
+	compilers: {
+		'c++': 'gsnapshot',
+		'c': 'cgsnapshot'
+	},
+	flags: {
+		'gsnapshot': '-std=c++26 -freflection -Wall -Wextra -Wpedantic -Wconversion -Wsign-conversion',
+		'clang_trunk': '-std=c++26 -Wall -Wextra -Wpedantic -Wconversion -Wsign-conversion',
+		'cgsnapshot': '-std=c2y -Wall -Wextra -Wpedantic -Wconversion -Wsign-conversion'
+	},
+	...(JSON.parse(localStorage.getItem('stuff')) ?? {})
+};
 
-require.config({ paths: { 'vs': 'https://cdn.jsdelivr.net/npm/monaco-editor@0.39.0/min/vs' }});
-require(['vs/editor/editor.main'], () => {
-	let compiling = false;
+const monaco_langs = {
+	"c": "c",
+	"c++": "cpp"
+};
+
+require(['vs/editor/editor.main'], async () => {
+	if (new URL(location).searchParams.has('z')) {
+		try {
+			const session = (await (await fetch(`https://godbolt.org/api/shortlinkinfo/${new URL(location).searchParams.get('z')}`)).json()).sessions[0];
+			config.code = session.source;
+			config.lang = session.language;
+			if (session.compilers.length) {
+				config.compilers[config.lang] = session.compilers[0].id;
+			}
+			for (const compiler of session.compilers) {
+				config.flags[compiler.id] = compiler.options;
+			}
+		} catch {}
+	}
+	flags.value = config.flags[config.compilers[config.lang]];
+
+	const langs = await (await fetch('https://godbolt.org/api/languages', { headers: { 'Accept': 'application/json' } })).json();
+	for (const lang of langs) {
+		const option = document.createElement('option');
+		option.innerText = lang.name;
+		if (lang.id === config.lang) {
+			option.setAttribute('selected', '');
+		}
+		lang_select.appendChild(option);
+	}
+	let compilers = {};
+	const editor = monaco.editor.create(source, {
+		language: monaco_langs[config.lang] ?? langs.find(({ id }) => id == config.lang).monaco,
+		value: config.code,
+		theme: 'vs-dark',
+		automaticLayout: true,
+		minimap: { enabled: false }
+	});
+	async function update_compilers_list() {
+		compilers = await (await fetch(`https://godbolt.org/api/compilers/${config.lang}`, { headers: { 'Accept': 'application/json' } })).json();
+		while (compiler_select.lastElementChild) {
+			compiler_select.removeChild(compiler_select.lastElementChild);
+		}
+		for (const compiler of compilers) {
+			const option = document.createElement('option');
+			option.innerText = compiler.name;
+			if (compiler.id === config.compilers[config.lang]) {
+				option.setAttribute('selected', '');
+			}
+			compiler_select.appendChild(option);
+		}
+	}
+	let current_promise;
+	let next_promise;
 	async function compile() {
-		if (compiling) {
-			return;
+		function get_promise() {
+			return new Promise(async (resolve) => {
+				status.innerText = 'Compiling...';
+				const result = await (await fetch(`https://godbolt.org/api/compiler/${config.compilers[config.lang]}/compile`, {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+						'Accept': 'application/json'
+					},
+					body: JSON.stringify({
+						source: config.code,
+						options: {
+							userArguments: config.flags[config.compilers[config.lang]],
+							compilerOptions: {
+								skipAsm: true
+							},
+							filters: {
+								execute: true
+							}
+						}
+					})
+				})).json();
+				let output_text = [];
+				if (result.stderr.length) {
+					output_text.push(result.stderr.map(({ text }) => text).join('\n'));
+				}
+				if (result.timedOut) {
+					output_text.push('<Compiler timed out>');
+				} else if (result.code) {
+					output_text.push(`Compiler returned: ${result.code}`);
+				} else if (result.execResult?.timedOut) {
+					output_text.push('<Program timed out>');
+				} else if (result.execResult) {
+					output_text.push(result.execResult.stdout.map(({ text }) => text).join('\n') || '<No program output>');
+					output_text.push(`Program returned: ${result.execResult.code}`);
+				}
+				output.innerHTML = new AnsiUp().ansi_to_html(output_text.join('\n\n'));
+				status.innerText = 'Output';
+				resolve();
+			});
 		}
-		compiling = true;
-		const code = editor.getValue().trim();
-		const url = new URL(location);
-		if (code.length) {
-			url.searchParams.set('z', btoa(JSON.stringify({ code, flags: flags.value })));
-			history.replaceState({}, '', url);
+		config.code = editor.getValue().trim();
+		localStorage.setItem('stuff', JSON.stringify({
+			code: config.code,
+			lang: config.lang,
+			compiler: config.compilers,
+			flags: config.flags
+		}));
+		if (!current_promise) {
+			current_promise = get_promise();
+			await current_promise;
+			current_promise = next_promise;
+			next_promise = null;
+			await current_promise;
 		} else {
-			url.searchParams.delete('z');
-			history.replaceState({}, '', url);
+			next_promise = get_promise();
 		}
-		status.innerText = 'Compiling...';
-		const result = await (await fetch('https://godbolt.org/api/compiler/gsnapshot/compile', {
+	}
+	const compile_debounce = debounce(1000, compile);
+
+	lang_select.addEventListener('change', async () => {
+		config.lang = langs[lang_select.selectedIndex].id;
+		update_compilers_list();
+		editor.getModel().setLanguage(monaco_langs[config.lang] ?? langs[lang_select.selectedIndex].monaco);
+		await compile();
+	});
+	compiler_select.addEventListener('change', async () => {
+		config.compilers[config.lang] = compilers[compiler_select.selectedIndex].id;
+		flags.value = config.flags[config.compilers[config.lang]] ?? '';
+		await compile();
+	});
+	flags.addEventListener('change', () => {
+		config.flags[config.compilers[config.lang]] = flags.value ?? '';
+		compile_debounce();
+	});
+	editor.onDidChangeModelContent(() => {
+		const url = new URL(location);
+		url.searchParams.delete('z');
+		history.replaceState({}, '', url);
+		compile_debounce();
+	});
+	recompile.addEventListener('click', async () => await compile());
+	short_link.addEventListener('click', async () => {
+		const { url } = await (await fetch('https://godbolt.org/api/shortener', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
 				'Accept': 'application/json'
 			},
 			body: JSON.stringify({
-				source: code,
-				options: {
-					userArguments: `${flags.value} -fdiagnostics-color=never`,
-					compilerOptions: {
-						skipAsm: true
-					},
-					filters: {
-						execute: true
+				sessions: [
+					{
+						language: config.lang,
+						source: config.code,
+						compilers: [
+							{
+								id: config.compilers[config.lang],
+								options: config.flags[config.compilers[config.lang]],
+								filter: {
+									execute: true
+								},
+								libs: [] // TODO
+							},
+						]
 					}
-				}
+				]
 			})
 		})).json();
-		let output_text = [];
-		if (result.stderr.length) {
-			output_text.push(result.stderr.map(({ text }) => text).join('\n'));
-		}
-		if (result.timedOut) {
-			output_text.push('<Compiler timed out>');
-		} else if (result.code) {
-			output_text.push(`Compiler returned: ${result.code}`);
-		} else if (result.execResult?.timedOut) {
-			output_text.push('<Program timed out>');
-		} else if (result.execResult) {
-			output_text.push(result.execResult.stdout.map(({ text }) => text).join('\n') || '<No program output>');
-			output_text.push(`Program returned: ${result.execResult.code}`);
-		}
-		output.innerText = output_text.join('\n\n');
-		status.innerText = 'Output';
-		compiling = false;
-	}
-	const compile_debounce = debounce(1000, compile);
+		navigator.clipboard.writeText(url);
 
-	let initial = {};
-	try {
-		initial = JSON.parse(atob(new URL(location).searchParams.get('z') || '{}'));
-	} catch {}
-	flags.value = initial.flags ?? '-std=c++26 -freflection -Wall -Wextra -Wpedantic -Wconversion -Wsign-conversion';
-	const source = document.querySelector('#source');
-	const editor = monaco.editor.create(source, {
-		language: 'cpp',
-		value: initial.code,
-		theme: 'vs-dark',
-		automaticLayout: true,
-		minimap: { enabled: false }
+		const otherthing = new URL(location);
+		otherthing.searchParams.set('z', url.split('/').at(-1));
+		history.replaceState({}, '', otherthing);
+
+		alert(`Saved to clipboard!`);
 	});
-	compile(initial.code, flags.value);
-	source.addEventListener('change', async () => {
-		await compile_debounce(editor.getValue(), flags.value);
-	});
-	flags.addEventListener('change', async () => {
-		await compile_debounce(editor.getValue(), flags.value);
-	});
-	status.addEventListener('click', async () => {
-		await compile(editor.getValue(), flags.value);
-	});
+
+	await update_compilers_list();
+	await compile();
 });
--- server.mjs
+++ server.mjs
@@ -2,8 +2,17 @@ import { createServer } from 'node:http';
 import { createReadStream } from 'node:fs';
 import { join, parse } from 'node:path';
 
-createServer((req, res) =>
-	createReadStream(join('./', (req.url.split('?')[0] == '/') ? 'index.html' : (req.url.split('?')[0] + (parse(req.url).ext ? '' : '.html'))))
-		.on('open', function() { this.pipe(res); })
+createServer((req, res) => {
+	const path = join('./', (req.url.split('?')[0] == '/') ? 'index.html' : (req.url.split('?')[0] + (parse(req.url).ext ? '' : '.html')));
+	createReadStream(path)
 		.on('error', () => res.writeHead(404).end())
-).listen(8029);
+		.on('open', function() {
+			res.setHeader('Content-Type', {
+				css: 'text/css',
+				html: 'text/html',
+				js: 'application/javascript',
+				png: 'image/png'
+			}[parse(path).ext.slice(1)]);
+			this.pipe(res);
+		});
+}).listen(8029);
